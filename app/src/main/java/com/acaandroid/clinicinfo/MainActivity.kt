afa4e64147d011490b5e472b98dcde30002d816b
Arthur <dev.arthur.vardanyan@gmail.com>
2020-10-03 17:59:59 +0400

    bottomnavigation

--- app/src/main/java/com/acaandroid/clinicinfo/MainActivity.kt
+++ app/src/main/java/com/acaandroid/clinicinfo/MainActivity.kt
@@ -3,13 +3,26 @@ package com.acaandroid.clinicinfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this,"I added androidx.core:core-ktx:1.3.2 dependency",Toast.LENGTH_SHORT).show()
-        Toast.makeText(this,"I added androidx.core:core-ktx:1.3.2 dependency",Toast.LENGTH_SHORT).show()
+        bottom_navigation.setOnNavigationItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.list -> {
+                    Toast.makeText(this, "Patient List", Toast.LENGTH_SHORT).show()
+                }
+                R.id.add -> {
+                    Toast.makeText(this, "Add List", Toast.LENGTH_SHORT).show()
+                }
+                R.id.dataBase -> {
+                    Toast.makeText(this, "DataBase List", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     }
 }
