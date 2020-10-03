3a090d794ea2106d9408d8701469ff45f31cb5e6
sargi <sargiss@gmail.com>
2020-10-03 13:32:15 +0400

    add dependency android ktx

--- app/src/main/java/com/acaandroid/clinicinfo/MainActivity.kt
+++ app/src/main/java/com/acaandroid/clinicinfo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.acaandroid.clinicinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"I added androidx.core:core-ktx:1.3.2 dependency",Toast.LENGTH_SHORT).show()
     }
 }
