da3c9510a3a5c85c2e2370e2ae67502f5fd4344f
Arthur <dev.arthur.vardanyan@gmail.com>
2020-10-17 11:54:51 +0400

    Delete Home Page

--- app/src/main/java/com/acaandroid/clinicinfo/MainActivity.kt
+++ app/src/main/java/com/acaandroid/clinicinfo/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     private var fragmentRegisterPatient: RegisterPatient = RegisterPatient()
     private var fragmentListPatent: ListPatient = ListPatient()
     private var fragmentDataBasePatient: DataBasePatient = DataBasePatient()
-    private var fragmentHomePage: HomePage = HomePage();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +16,6 @@ class MainActivity : AppCompatActivity() {
 
         bottom_navigation.setOnNavigationItemReselectedListener { item ->
             when(item.itemId) {
-                R.id.home -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment, fragmentHomePage)
-                        .commit()
-                }
-
                 R.id.list -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fragment, fragmentListPatent)
