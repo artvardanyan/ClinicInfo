1474d4526a06b94848452923a607dac8cbec64e0
Arthur <dev.arthur.vardanyan@gmail.com>
2020-10-17 12:20:27 +0400

    Add recording

--- app/src/main/java/com/acaandroid/clinicinfo/ListPatient.kt
+++ app/src/main/java/com/acaandroid/clinicinfo/ListPatient.kt
@@ -38,7 +38,6 @@ class ListPatient : Fragment() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-
     }
 
     companion object {
